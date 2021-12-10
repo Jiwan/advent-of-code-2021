23a0e5b988f9031d8b6248e5855bb39ee915b892
Jean Guegant <jguegant@gmail.com>
2021-12-10 21:47:46 +0100

    Day 10.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,123 +1,82 @@
 use itertools::Itertools;
-use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-struct Heightmap {
-    data: Vec<Vec<i32>>,
-}
-
-impl Heightmap {
-    fn size(&self) -> (usize, usize) {
-        (self.data[0].len(), self.data.len())
-    }
-
-    #[rustfmt::skip]
-    fn adjacent_points(&self, pos: (usize, usize)) -> [Option<(usize, usize)>; 4] {
-        [
-            if pos.0 > 0 { Some((pos.0 - 1, pos.1)) } else { None },
-            if pos.1 > 0 { Some((pos.0, pos.1 - 1)) } else { None },
-            if pos.0 < self.size().0 - 1 { Some((pos.0 + 1, pos.1)) } else { None },
-            if pos.1 < self.size().1 - 1 { Some((pos.0, pos.1 + 1)) } else { None },
-        ]
-    }
-
-    fn get_height(&self, pos: (usize, usize)) -> i32 {
-        self.data[pos.1][pos.0]
-    }
-
-    fn find_low_points(&self) -> Vec<(usize, usize)> {
-        self.data
-            .iter()
-            .enumerate()
-            .map(|(y, row)| {
-                row.iter()
-                    .enumerate()
-                    .filter(move |(x, value)| {
-                        self.adjacent_points((*x, y))
-                            .iter()
-                            .all(|point| point.map_or(true, |p| **value < self.get_height(p)))
-                    })
-                    .map(move |(x, _)| (x, y))
-            })
-            .flatten()
-            .collect()
-    }
-}
-
-fn parse(path: &'static str) -> Heightmap {
+fn parse(path: &'static str) -> Vec<String> {
     let file = File::open(path).expect("Couldn't open data file");
     let lines = BufReader::new(file).lines();
+    lines.flatten().collect()
+}
 
-    let data = lines
-        .map(|line| {
-            line.unwrap()
-                .chars()
-                .map(|c| c.to_digit(10).unwrap() as i32)
-                .collect()
-        })
-        .collect();
-    Heightmap { data }
+const PAIRS: [(char, char); 4] = [('<', '>'), ('(', ')'), ('[', ']'), ('{', '}')];
+
+const ERROR_SCORE: [(char, i32); 4] = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)];
+
+const COMPLETION_SCORE: [(char, i32); 4] = [(')', 1), (']', 2), ('}', 3), ('>', 4)];
+
+fn get_error_score(c: char) -> i32 {
+    ERROR_SCORE.iter().find(|(c2, _)| c == *c2).unwrap().1
+}
+
+fn get_completion_score(c: char) -> i32 {
+    COMPLETION_SCORE.iter().find(|(c2, _)| c == *c2).unwrap().1
+}
+
+enum ChunkParsingResult {
+    Corrupted(char),
+    Incomplete(Vec<char>),
+}
+
+fn parse_chunk(chunk: &str) -> ChunkParsingResult {
+    let mut state: Vec<char> = Vec::new();
+
+    for c in chunk.chars() {
+        if let Some((_, p2)) = PAIRS.iter().find(|(p1, _)| *p1 == c) {
+            state.push(*p2);
+        } else if let Some(expected) = state.pop() {
+            if c != expected {
+                return ChunkParsingResult::Corrupted(c);
+            }
+        } else {
+            return ChunkParsingResult::Corrupted(c);
+        }
+    }
+
+    ChunkParsingResult::Incomplete(state)
 }
 
 fn part1() {
-    let heightmap = parse("data/part1.txt");
+    let chunks = parse("data/part1.txt");
 
-    let risk_level: i32 = heightmap
-        .find_low_points()
+    let result = chunks
         .iter()
-        .map(|p| heightmap.get_height(*p) + 1)
-        .sum();
-    println!("{}", risk_level);
-}
+        .map(|s| parse_chunk(s))
+        .filter_map(|r| match r {
+            ChunkParsingResult::Corrupted(c) => Some(get_error_score(c)),
+            _ => None,
+        })
+        .sum::<i32>();
 
-#[derive(Default)]
-struct BasinCrawler {
-    visited_point: HashSet<(usize, usize)>,
-}
-
-impl BasinCrawler {
-    fn new() -> Self {
-        Default::default()
-    }
-
-    fn crawl(&mut self, pos: (usize, usize), heightmap: &Heightmap) -> usize {
-        if self.visited_point.contains(&pos) {
-            return 0;
-        }
-
-        self.visited_point.insert(pos);
-
-        if heightmap.get_height(pos) >= 9 {
-            return 0;
-        }
-
-        let mut size = 1;
-        for p in heightmap.adjacent_points(pos).iter().flatten() {
-            size += self.crawl(*p, heightmap);
-        }
-
-        size
-    }
+    println!("{}", result);
 }
 
 fn part2() {
-    let heightmap = parse("data/part1.txt");
+    let chunks = parse("data/part1.txt");
 
-    let basin: usize = heightmap
-        .find_low_points()
+    let result = chunks
         .iter()
-        .map(|p| {
-            let mut crawler = BasinCrawler::new();
-            crawler.crawl(*p, &heightmap)
+        .map(|s| parse_chunk(s))
+        .filter_map(|r| match r {
+            ChunkParsingResult::Incomplete(v) => Some(v.iter().rev().fold(0u64, |result, c| {
+                (result * 5) + (get_completion_score(*c) as u64)
+            })),
+            _ => None,
         })
         .sorted()
-        .rev()
-        .take(3)
-        .product();
+        .collect::<Vec<_>>();
 
-    println!("{}", basin);
+    println!("{:?}", result[result.len() / 2]);
 }
 
 fn main() {
